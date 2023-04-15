837920d1847df5f19bc141f6c1c7d4afe626c4eb
gonghuxuan <741247407@qq.com>
2023-04-15 15:11:54 +0800

    修改新高异动

--- src/renderer/views/dataV/quan/quan-bankuai.vue
+++ src/renderer/views/dataV/quan/quan-bankuai.vue
@@ -1,49 +1,47 @@
 <template>
-  <div class="jubu-bankuai">
-    <div class="sn-container sn-container-1">
-      <dv-border-box-10 :color="['#5D9A9E', '#0694B1']">
-        <div class="juzhong font-18 bankuai-title">涨停板块个数1
+    <div class="jubu-bankuai">
+        <div class="sn-container sn-container-1">
+            <dv-border-box-10 :color="['#5D9A9E', '#0694B1']">
+                <div class="juzhong font-18 bankuai-title">涨停板块个数1 </div>
+            </dv-border-box-10>
         </div>
-      </dv-border-box-10>
-    </div>
-    <div class="sn-container sn-container-2">
-      <dv-border-box-10 :color="['#5D9A9E', '#0694B1']">
-        <div class="juzhong font-18 bankuai-title">
-          <quanxingao />
+        <div class="sn-container sn-container-2">
+            <dv-border-box-10 :color="['#5D9A9E', '#0694B1']">
+                <div class="juzhong font-18 bankuai-title">
+                    <quanxingao stockType="0" title="新高异动" />
+                </div>
+            </dv-border-box-10>
         </div>
-      </dv-border-box-10>
-    </div>
-    <div class="sn-container sn-container-3">
-      <dv-border-box-10 :color="['#5D9A9E', '#0694B1']">
-        <div class="juzhong font-18 bankuai-title">
-          <bankuaiitem title="平台突破">
-            <quanpingtai />
-          </bankuaiitem>
+        <div class="sn-container sn-container-3">
+            <dv-border-box-10 :color="['#5D9A9E', '#0694B1']">
+                <div class="juzhong font-18 bankuai-title">
+                    <bankuaiitem title="平台突破">
+                        <quanxingao stockType="1" title="平台突破" />
+                    </bankuaiitem>
+                </div>
+            </dv-border-box-10>
         </div>
-      </dv-border-box-10>
-    </div>
-    <div class="sn-container sn-container-4">
-      <dv-border-box-10 :color="['#5D9A9E', '#0694B1']">
-        <div class="juzhong font-18 bankuai-title">
-          <bankuaiitem title="游资股预警">
-            <quanyouzi />
-          </bankuaiitem>
+        <div class="sn-container sn-container-4">
+            <dv-border-box-10 :color="['#5D9A9E', '#0694B1']">
+                <div class="juzhong font-18 bankuai-title">
+                    <bankuaiitem title="游资股预警">
+                        <quanxingao stockType="3" title="游资股预警" />
+                    </bankuaiitem>
+                </div>
+            </dv-border-box-10>
         </div>
-      </dv-border-box-10>
-    </div>
-    <div class="sn-container sn-container-5">
-      <dv-border-box-10 :color="['#0694B1', '#5D9A9E']">
-        <div class="juzhong font-18 bankuai-title">连板高度预警
+        <div class="sn-container sn-container-5">
+            <dv-border-box-10 :color="['#0694B1', '#5D9A9E']">
+                <div class="juzhong font-18 bankuai-title">连板高度预警 </div>
+            </dv-border-box-10>
         </div>
-      </dv-border-box-10>
     </div>
-  </div>
 </template>
 
 <script>
 import quanxingao from "./quan/quan-xingao.vue";
-import quanpingtai from "./quan/quan-pingtai.vue";
-import quanyouzi from "./quan/quan-youzi.vue";
+// import quanpingtai from "./quan/quan-pingtai.vue";
+// import quanyouzi from "./quan/quan-youzi.vue";
 
 import bankuaiitem from "@/views/dataV/components/bankuai-item.vue";
 
@@ -53,9 +51,6 @@ export default {
     },
     components: {
         quanxingao,
-        quanpingtai,
-        quanyouzi,
-        bankuaiitem,
     },
     computed: {},
     created() {},
