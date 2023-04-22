33b8d976f744121f986150420fe75e1b42595afb
gonghuxuan <741247407@qq.com>
2023-04-23 00:27:09 +0800

    散点

--- src/renderer/views/dataV/jubu/jubu/jubu-zhishubankuai.vue
+++ src/renderer/views/dataV/jubu/jubu/jubu-zhishubankuai.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="jubu-zhishubankuai">
-    <div class="juzhong">{{ title }}
+    <div class="juzhong">
       <img @click="toDetail" src="@/assets/img/detail.svg" alt="" class="detail" style="width: 20px; cursor: pointer" />
     </div>
     <div class="table-contain">
-
+      <div id="charts-zhishubankuai" style="height: 500px; width: 100%; margin-top: 0px"></div>
     </div>
   </div>
 </template>
@@ -18,31 +18,6 @@ export default {
     data() {
         return {
             resData: [],
-            resDataQianbu: [],
-            resDataZhongbu: [],
-            resDataHoubu: [],
-            columns: [
-                {
-                    title: "名称",
-                    dataIndex: "stockName",
-                    key: "stockName",
-                    align: "center",
-                },
-                {
-                    title: "价格",
-                    dataIndex: "close",
-                    key: "close",
-                    scopedSlots: { customRender: "close" },
-                    align: "center",
-                },
-                {
-                    title: "涨跌幅",
-                    dataIndex: "changepercent",
-                    key: "changepercent",
-                    align: "center",
-                    scopedSlots: { customRender: "changepercent" },
-                },
-            ],
         };
     },
     props: {
@@ -60,7 +35,220 @@ export default {
         getData() {
             getPlateChangepercentData({}).then((res) => {
                 console.log("指数板块-----", res);
+                this.resData = res;
+                this.setChart();
             });
+        },
+        setChart() {
+            this.chart = echarts.init(
+                document.getElementById("charts-zhishubankuai")
+            );
+            const option = {
+                xAxis: {
+                    splitLine: {
+                        show: false,
+                    },
+                    axisTick: {
+                        //y轴刻度线
+                        show: false,
+                    },
+                    axisLine: {
+                        show: false,
+                    },
+                    show: false,
+                },
+                yAxis: {
+                    splitLine: {
+                        show: false,
+                    },
+
+                    axisLine: {
+                        show: true,
+                    },
+                    axisTick: {
+                        show: false,
+                    },
+                },
+                tooltip: {
+                    trigger: "axis",
+                    axisPointer: {
+                        type: "cross",
+                        label: {
+                            backgroundColor: "#6a7985",
+                        },
+                    },
+                },
+                series: [
+                    {
+                        symbolSize: 10,
+                        data: [
+                            [96.24, 11.35, "张三"],
+                            [33.09, 85.11, "李四"],
+                            [57.6, 36.61, "王五"],
+                            [36.77, 27.26, "张三"],
+                            [20.1, 6.72, "张三"],
+                            [45.53, 36.37, "张三"],
+                            [110.07, 80.13, "张三"],
+                            [72.05, 20.88, "张三"],
+                            [39.82, 37.15, "张三"],
+                            [48.05, 70.5, "张三"],
+                            [0.85, 2.57, "张三"],
+                            [51.66, 63.7, "张三"],
+                            [61.07, 127.13, "张三"],
+                            [64.54, 33.59, "张三"],
+                            [35.5, 25.01, "张三"],
+                            [226.55, 664.02, "张三"],
+                            [188.6, 175.31, "张三"],
+                            [81.31, 108.68, "张三"],
+                        ],
+                        markLine: {
+                            symbol: "none", //去掉警戒线最后面的箭头
+                            label: {
+                                position: "start", //将警示值放在哪个位置，三个值“start”,"middle","end"  开始  中点 结束
+                            },
+                            data: [
+                                {
+                                    silent: true, //鼠标悬停事件  true没有，false有
+                                    lineStyle: {
+                                        //警戒线的样式  ，虚实  颜色
+                                        // type: "dashed",
+                                        // color: "#5D9A9E",
+                                        normal: {
+                                            lineStyle: {
+                                                type: "dashed",
+                                                color: "#5D9A9E",
+                                                // type: 'solid'  //'dotted'虚线 'solid'实线
+                                            },
+                                            label: {
+                                                show: false,
+                                                position: "center",
+                                                //  formatter: '{a}',
+                                            },
+                                        },
+                                    },
+                                    yAxis: this.resData.avgChangepercent, // 警戒线的标注值，可以有多个yAxis,多条警示线   或者采用   {type : 'average', name: '平均值'}，type值有  max  min  average，分为最大，最小，平均值
+                                },
+                            ],
+                        },
+                        type: "scatter",
+                    },
+                ],
+            };
+            const data = [
+                [96.24, 11.35, "张三"],
+                [33.09, 85.11, "李四"],
+                [57.6, 36.61, "王五"],
+                [36.77, 27.26, "张三"],
+                [20.1, 6.72, "张三"],
+                [45.53, 36.37, "张三"],
+                [110.07, 80.13, "张三"],
+                [72.05, 20.88, "张三"],
+                [39.82, 37.15, "张三"],
+                [48.05, 70.5, "张三"],
+                [0.85, 2.57, "张三"],
+                [51.66, 63.7, "张三"],
+                [61.07, 127.13, "张三"],
+                [64.54, 33.59, "张三"],
+                [35.5, 25.01, "张三"],
+                [226.55, 664.02, "张三"],
+                [188.6, 175.31, "张三"],
+                [81.31, 108.68, "张三"],
+            ];
+
+            const option2 = {
+                tooltip: {
+                    trigger: "axis",
+                    axisPointer: {
+                        type: "cross",
+                    },
+                },
+                title: {
+                    text: "18 companies net profit and main business income (million)",
+                    subtext: "By ecStat.regression",
+                    sublink: "https://github.com/ecomfe/echarts-stat",
+                    left: "center",
+                    top: 16,
+                },
+                xAxis: {
+                    type: "category",
+                    min: "value",
+                },
+                yAxis: {
+                    type: "value",
+                    min: 0,
+                    splitLine: {
+                        lineStyle: {
+                            type: "dashed",
+                        },
+                    },
+                },
+                grid: {
+                    top: 90,
+                },
+                series: [
+                    {
+                        name: "scatter",
+                        type: "scatter",
+                        label: {
+                            emphasis: {
+                                show: true,
+                                position: "right",
+                                textStyle: {
+                                    color: "blue",
+                                    fontSize: 16,
+                                },
+                            },
+                        },
+                        itemStyle: {
+                            show: true,
+                            markPoint: {
+                                tooltip: {
+                                    show: true,
+                                    formatter: "z",
+                                },
+                            },
+                        },
+                        data: data,
+                        itemStyle: {
+                            normal: {
+                                label: {
+                                    position: [5, 9],
+                                    show: true,
+                                    formatter: function (params) {
+                                        return params.value[2];
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    {
+                        name: "示例",
+                        type: "line",
+                        data: [60],
+                        markLine: {
+                            data: [{ type: "average", name: "平均值" }],
+                        },
+                    },
+                    {
+                        data: [120, 200, 150, 80, 70, 110, 130],
+                        type: "line",
+                        symbol: "pin",
+                        symbolSize: 10,
+                        lineStyle: {
+                            normal: {
+                                color: "blue",
+                                width: 4,
+                                type: "dashed",
+                            },
+                        },
+                        itemStyle: { normal: { label: { show: true } } },
+                    },
+                ],
+            };
+
+            console.log("111111111111111111option11");
+            this.chart.setOption(option2);
+            console.log("111111111111111111option11222222222222");
         },
         toDetail() {
             this.$router.push({
